7a88a3e1c4c0702b531efcd8a26eff13239fd70c
duongtm <tmd06forwork@gmail.com>
2026-03-29 22:50:24 +0700

    Update posts.js

--- functions/api/posts.js
+++ functions/api/posts.js
@@ -1,1 +1,19 @@
 
+export async function onRequestGet(context) {
+  const url = new URL(context.request.url);
+  const postsUrl = new URL('/posts.json', url.origin);
+
+  const res = await context.env.ASSETS.fetch(postsUrl);
+  if (!res.ok) {
+    return new Response(JSON.stringify({ error: 'cannot load posts.json' }), {
+      status: 500,
+      headers: { 'content-type': 'application/json; charset=utf-8' }
+    });
+  }
+
+  const posts = await res.json();
+
+  return new Response(JSON.stringify(posts), {
+    headers: { 'content-type': 'application/json; charset=utf-8' }
+  });
+}
